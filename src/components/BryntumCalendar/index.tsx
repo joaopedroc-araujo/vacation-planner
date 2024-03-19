d1511698599f7d2b25efc6c3f9e2268493147d2d
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2024-03-19 14:04:08 -0300

    fix: change to Bryntum Calendar

--- src/components/BryntumCalendar/index.tsx
+++ src/components/BryntumCalendar/index.tsx
@@ -6,34 +6,27 @@ const Calendar = () => {
             timeRanges: true,
             eventEdit: {
                 items: {
-                    locationField: {
-                        type: 'text',
-                        name: 'location',
-                        label: 'Location',
-                        required: true,
-                        colorField: true,
-                    },
                     descriptionField: {
                         type: 'textArea',
                         name: 'description',
                         label: 'Description',
                         required: true,
-                        colorField: true,
+                    },
+                    locationField: {
+                        type: 'text',
+                        name: 'location',
+                        label: 'Location',
+                        required: true,
                     },
                     participantsField: {
                         type: 'text',
                         name: 'participants',
-                        required: true,
                         label: 'Participants',
-                        colorField: true,
                     },
-                    checkbox: false,
                     startTimeField: false,
                     endTimeField: false,
                     resourceField: false,
                     recurrenceCombo: false,
-                    colorField: true,
-                    allDayField: false,
                 }
             }
         },
